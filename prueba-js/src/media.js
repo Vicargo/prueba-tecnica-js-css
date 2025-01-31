6e95133723e4a0ae3e2fff109e0588e3add074d5
Victor Arnau <varnau@vottun.com>
2025-01-31 21:29:22 +0100

    Update media.js

--- prueba-js/src/media.js
+++ prueba-js/src/media.js
@@ -1,5 +1,18 @@
+/**
+ * @notice Funcion que recibe un json de N personas con sus edades y devuelve la edad media de todas las personas.
+ * @param json
+ * @return string
+ * 
+ */
 export default (json) => {
-    // Do your thing here!
-    return "";
+    /** 
+      @notice La funcion reduce nos permite recorrer un array a la vez que acumular un valor, el segundo argumento que recibe es el valor inicial del acumulador. En nuestro caso utilizamos este acumulador para ir sumando las edades de cada persona. 
+    */
+    const total = json.reduce((acc, item) => acc + item.edad, 0);
+    
+    /**
+     * @notice Para obtener la media hacemos la división del acumulador entre todas las personas que hay en el json
+     */
+    return `${total / json.length} años de media`;
   };
   
